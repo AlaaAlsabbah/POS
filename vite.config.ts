dcf1bddc1d042a4af90968ce47c03ef173a3d423
AlaaAlsabbah <110497040+AlaaAlsabbah@users.noreply.github.com>
2026-01-03 00:09:35 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/POS-UI/', // Change this to your repo name
+  base: '/POS/', // Your GitHub repo name
 })
 
